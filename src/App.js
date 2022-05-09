8fb64c6db53bd3231d19ea6612a13682d4e30cc6
umuhoza faith <umuhozafaith@umuhozas-MacBook-Pro.local>
2022-05-09 10:40:28 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+import {motion} from 'framer-motion'
+import {useRef, useEffect, useState} from 'react'
+import images from './assets/Images';
+
+function App() {
+  const [width, setWidth] = useState(0);
+
+  const carousel = useRef();
+
+  useEffect(() => {
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth)
+  }, [])
+
+  return (
+    <div>
+      <motion.div ref={carousel} className='carousel border-gradient'>
+        <motion.div 
+        drag='x'
+        whileTap={{cursor: "grabbing"}} 
+        dragConstraints={{right: 0, left: -width}} 
+        className='inner-carousel'>
+          {images.map((image, index) => {
+            return (
+              <motion.div className='item'>
+                <img 
+                key={index}
+                src={image}
+                alt="New York"
+                />
+              </motion.div>
+            );
+          })}
+        </motion.div>
+      </motion.div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/assets/Images.js
@@ -0,0 +1,10 @@
+import image1 from '../assets/asset-1.jpg'
+import image2 from '../assets/asset-2.jpg'
+import image3 from '../assets/asset-3.jpg'
+import image4 from '../assets/asset-4.jpg'
+import image5 from '../assets/asset-5.jpg'
+import image6 from '../assets/asset-6.jpg'
+
+const images = [image1, image2, image3, image4, image5, image6];
+
+export default images;
